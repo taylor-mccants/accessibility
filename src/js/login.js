1d1182ec38249ad0c6a50a7cde365062561ad4fd
Taylor McCants <taylor.mccants@uzh.ch>
2021-04-02 23:49:36 +0200

    add validity messages to form

--- src/js/login.js
+++ src/js/login.js
@@ -35,30 +35,32 @@ function login(event) {
 
     var hasError = false;
 
+    //TODO: Think of a better way than nextElementSibling.
     var email = document.getElementById('login-email-control');
     if (email.validity.valid) {
         setValid(email);
+        email.nextElementSibling.innerHTML = '';
     } else if (email.validity.valueMissing) {
         setInvalid(email);
+        email.nextElementSibling.innerHTML = 'Email cannot be empty.';
         hasError = true;
     } else {
         setInvalid(email);
+        email.nextElementSibling.innerHTML = 'Invalid email.';
         hasError = true;
     }
 
     var password = document.getElementById('login-password-control');
-    if (password.value.trim().length == 0) {
+    if (password.value.trim().length === 0) {
         setInvalid(password);
+        password.nextElementSibling.innerHTML = 'Password cannot be empty.';
         hasError = true;
     } else {
         setValid(password);
+        password.nextElementSibling.innerHTML = '';
     }
 
-    if (hasError) {
-        document.getElementById('login-error').classList.remove('d-none');
-    } else {
-        document.getElementById('login-error').classList.add('d-none');
-    }
+    setGeneralErrorMessage('login-error', hasError);
 }
 
 /**
@@ -71,11 +73,14 @@ function forgot(event) {
 
     var hasError = false;
 
+    //TODO: Think of a better way than nextElementSibling.
     var email = document.getElementById('login-email-control');
     if (email.validity.valid) {
         setValid(email);
+        email.nextElementSibling.innerHTML = '';
     } else if (email.validity.valueMissing) {
         setInvalid(email);
+        email.nextElementSibling.innerHTML = 'Email cannot be empty.';
         hasError = true;
     } else {
         setInvalid(email);
@@ -85,11 +90,7 @@ function forgot(event) {
     var password = document.getElementById('login-password-control');
     removeValidation(password);
 
-    if (hasError) {
-        document.getElementById('login-error').classList.remove('d-none');
-    } else {
-        document.getElementById('login-error').classList.add('d-none');
-    }
+    setGeneralErrorMessage('login-error', hasError);
 }
 
 /**
@@ -102,30 +103,39 @@ function register(event) {
 
     var hasError = false;
 
+    //TODO: Think of a better way than nextElementSibling.
+
     var firstName = document.getElementById('register-first-name-control');
-    if (firstName.value.trim().length == 0) {
+    if (firstName.value.trim().length === 0) {
         setInvalid(firstName);
+        firstName.nextElementSibling.innerHTML = 'First name cannot be empty.';
         hasError = true;
     } else if (firstName.validity.valid) {
         setValid(firstName);
+        firstName.nextElementSibling.innerHTML = ''
     }
 
     var lastName = document.getElementById('register-last-name-control');
-    if (lastName.value.trim().length == 0) {
+    if (lastName.value.trim().length === 0) {
         setInvalid(lastName);
+        lastName.nextElementSibling.innerHTML = 'Last name cannot be empty.';
         hasError = true;
     } else if (lastName.validity.valid) {
         setValid(lastName);
+        lastName.nextElementSibling.innerHTML = '';
     }
 
     var email = document.getElementById('register-email-control');
     if (email.validity.valid) {
         setValid(email);
+        email.nextElementSibling.innerHTML = ''
     } else if (email.validity.valueMissing) {
         setInvalid(email);
+        email.nextElementSibling.innerHTML = 'Email cannot be empty.'
         hasError = true;
     } else {
         setInvalid(email);
+        email.nextElementSibling.innerHTML = 'Invalid email.'
         hasError = true;
     }
 
@@ -133,35 +143,66 @@ function register(event) {
     var passwordValue = password.value.trim();
     if (passwordValue.length < 8) {
         setInvalid(password);
+        password.nextElementSibling.innerHTML = 'Password must be at least 8 characters.'
         hasError = true;
     } else if (passwordValue.length > 16) {
         setInvalid(password);
+        password.nextElementSibling.innerHTML = 'Password must be less than 16 characters.'
         hasError = true;
     } else if (passwordValue.match(/[a-zA-Z]+/) == null) {
         setInvalid(password);
+        password.nextElementSibling.innerHTML = 'Password must contain at least one letter.'
         hasError = true;
     } else if (passwordValue.match(/[0-9]+/) == null) {
         setInvalid(password);
+        password.nextElementSibling.innerHTML = 'Password must contain at least one number.'
         hasError = true;
     } else {
         setValid(password);
+        password.nextElementSibling.innerHTML = ''
     }
 
     var programme = document.getElementById('register-programme-control');
     if (programme.validity.valueMissing) {
         setInvalid(programme);
+        programme.nextElementSibling.innerHTML = 'Programme cannot be empty.'
         hasError = true;
     } else if (!programme.validity.valid) {
         setInvalid(programme);
+        programme.nextElementSibling.innerHTML = 'Invalid programme.'
         hasError = true;
     } else {
         setValid(programme);
+        programme.nextElementSibling.innerHTML = ''
     }
 
+    setGeneralErrorMessage('register-error', hasError);
+}
+
+/**
+ * Focus the given element for editing
+ * @param {object} event - The DOM event
+ */
+function focusElement(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    //TODO: This is risky because if the html order changes, this is effected.
+    event.target.previousElementSibling.focus()
+}
+
+/**
+ * Set the general error message to appear or disappear according to error status
+ * @param {string} elementId - The error message element
+ * @param {boolean} hasError - The status of the form validation
+ */
+function setGeneralErrorMessage(elementId, hasError) {
+    var element = document.getElementById(elementId);
     if (hasError) {
-        document.getElementById('register-error').classList.remove('d-none');
+        element.classList.remove('d-none');
+        element.innerHTML = 'Please correct the error(s)';
     } else {
-        document.getElementById('register-error').classList.add('d-none');
+        element.classList.add('d-none');
+        element.innerHTML = '';
     }
 }
 
@@ -177,4 +218,10 @@ document.addEventListener('DOMContentLoaded', function() {
     document
         .getElementById('register-register-button')
         .addEventListener('click', register, false);
+
+    var invalidElements = document.getElementsByClassName("invalid-feedback");
+    for (var i = 0; i < invalidElements.length; i++) {
+        invalidElements[i].addEventListener('click', focusElement, false);
+    }
+
 }, false);
